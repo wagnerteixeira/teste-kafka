8c6a631ed713aed5ddb3c2c2ba172e01f3645e86
Wagner <wagnerbernardesteixeira@gmail.com>
2022-08-21 03:51:25 -0300

    ajustes versão kafka

--- kafka-broker-ssl-with-connect/auto/create-mongo-sink.sh
+++ kafka-broker-ssl-with-connect/auto/create-mongo-sink.sh
@@ -3,7 +3,7 @@ set -euf -o pipefail
 cd "$(dirname "$0"..)" || exit
 echo "🌲 Creating mongo sink connector"
 
-docker-compose exec mongo mongo --host mongodb://localhost:27017 config-data.js
-docker-compose exec broker kafka-topics --command-config /etc/kafka/config/command.properties --bootstrap-server=broker.local:19092 --create --topic UserEmail --partitions 1 --replication-factor 1
+docker-compose exec mongo mongo --host mongodb://mongo.local:27017 config-data.js
+docker-compose exec broker-3 kafka-topics --command-config /etc/kafka/config/command.properties --bootstrap-server="broker-1.local:19092,broker-2.local:19093,broker-3.local:19094" --create --topic UserEmail --partitions 3 --replication-factor 3
 curl -X POST -H "Content-Type: application/json" -d @sink-connector.json http://localhost:8083/connectors -w "\n"
 echo "Sink connector created! 👌"
